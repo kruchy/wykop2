f79ca2a781b259cb7be19e0791083bfa3ebc4300
Krzysztof Misiak <Krzysztof.Misiak@sabre.com>
2017-05-11 17:53:05 +0200

    Create registration testcases

--- server/test/tests.js
+++ server/test/tests.js
@@ -410,3 +410,49 @@ describe('Granting admin', function () {
             });
     });
 });
+
+
+describe('Registration tests',function () {
+    let server;
+    beforeEach(function () {
+        server = require('../app').server;
+        clearDatabase();
+    });
+    afterEach(function () {
+        clearDatabase();
+        server.close();
+
+    });
+    it('successfully register new user', function (done) {
+        chai.request(server)
+            .post('/register/')
+            .send({
+                username: 'Bruce',
+                password:'test',
+                email:'test@test.com'
+            })
+            .end(function (err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.have.property('success');
+                res.body.success.should.be.equal(true);
+                done()
+            });
+    });
+    it('Fails register new user due to invalid data', function (done) {
+        chai.request(server)
+            .post('/register/')
+            .send({
+                username: 'Bruce',
+                password: null,
+                email:'test@test.com'
+            })
+            .end(function (err, res) {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.body.should.have.property('success');
+                res.body.success.should.be.equal(false);
+                done()
+            });
+    })
+});
