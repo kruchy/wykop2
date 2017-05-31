a679e867bebd550eeb892a1a15b83abcfb9b62ff
Krzysztof Misiak <Krzysztof.Misiak@sabre.com>
2017-05-31 15:59:14 +0200

    Add subcomment template

---
+++ server/src/models/SubComment.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const User = require('./models').User;
+const Post = require('./models').Post;
+const Schema = mongoose.Schema;
+
+
+const subCommentSchema = new Schema({
+    author: {
+        type: Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    post: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
+
+}, { timestamps: { createdAt: 'created_at', updatedAt : 'updated_at' } });
+
+
+
+commentSchema.virtual("posts", {
+    ref: "Post",
+    localField: "_id",
+    foreignField: "comments"
+});
+
+
+module.exports.commentSchema = commentSchema;
