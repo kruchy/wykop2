3b0cf091d82559eee6fc82d665e6100b0db62067
Krzysztof Misiak <Krzysztof.Misiak@sabre.com>
2017-05-31 16:00:21 +0200

    Add subcomment template

---
+++ server/src/routes/reply.js
@@ -0,0 +1,149 @@
+`const express = require('express');
+const models = require('../models/models');
+const config = require("../../config.js");
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const sanitizeHtml = require('sanitize-html');
+
+
+router.delete("/", function (req, res) {
+    const commentId = req.body.commentId;
+    if (!commentId) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: 'Invalid comment id.'
+            });
+    }
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+        jwt.verify(token, config.secret, function (err, decoded) {
+            if (err) {
+                return res.status(401).json(
+                    {
+                        success: false,
+                        message: 'Failed to authenticate token.'
+                    });
+            } else {
+                models.Comment.findOne({_id: commentId}).populate('author').populate('posts').exec(function (err, comment) {
+                    if (err || !comment) {
+                        res.status(500)
+                            .json({success: false, error: "Problem retrieving comment from server", reason: err});
+                    }
+                    if (decoded._doc.username !== comment.author.username) {
+                        res.status(403).json(
+                            {
+                                success: false,
+                                message: 'Forbidden.'
+                            });
+                    }
+                    else {
+                        models.Post.update({replies: {_id: comment._id}}, {$pull: {comments: {_id: commentId}}}, {
+                                "new": true,
+                            },
+                            function (err, updatedPost) {
+                                comment.remove(function (err) {
+                                    if (err) {
+                                        res.status(500)
+                                            .json({
+                                                success: false,
+                                                message: 'Problem removing comment.',
+                                                reason: err
+                                            });
+                                    } else {
+                                        res.status(200)
+                                            .json({
+                                                success: true,
+                                                message: 'Deleted comment.'
+                                            })
+                                    }
+                                })
+
+                            });
+                    }
+                })
+            }
+        });
+    } else {
+        return res.status(403).json({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+
+});
+
+
+router.post("/", function (req, res) {
+    const content = sanitizeHtml(req.body.content);
+    const commentId = req.body.commentId;
+    if (!content || !postId) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid parameters provided.'
+        })
+    }
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+        jwt.verify(token, config.secret, function (err, decoded) {
+                if (err) {
+                    return res.status(401).json(
+                        {
+                            success: false,
+                            message: 'Failed to authenticate token.'
+                        });
+                } else {
+                    const comment = new models.Comment(
+                        {
+                            content: content,
+                            author: decoded._doc,
+                            parent: commentId
+                        }
+                    );
+
+                    comment.save(function (err) {
+                        if (err) {
+                            res.status(500).json(
+                                {
+                                    success: false,
+                                    message: 'Failed to save comment.',
+                                    reason: err
+                                }
+                            )
+                        }
+                        else {
+                            models.Comment.update({_id: commentId}, {$push: {replies: comment._id}}, {}, function (err) {
+                                if (err) {
+                                    res.status(500).json(
+                                        {
+                                            success: false,
+                                            message: 'Failed to update comment with reply.',
+                                            reason: err
+                                        }
+                                    )
+                                }
+                                else {
+                                    res.status(200).json({
+                                        success: true,
+                                        comment: comment
+                                    })
+                                }
+                            });
+                        }
+                    })
+                }
+            }
+        )
+
+    }
+    else {
+        return res.status(403).json({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+});
+
+module.exports = router;
---
+++ server/test/replyTests.js
@@ -0,0 +1,80 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const mongoose = require("mongoose");
+const utils = require('./testUtils');
+const clearDatabase = utils.clearDatabase;
+const createAndSaveUser = utils.createAndSaveUser;
+const createPostForUser = utils.createPostForUser;
+const createAdmin = utils.createAdmin;
+const createCommentForUser = utils.createCommentForUser;
+
+chai.use(chaiHttp);
+process.env.NODE_ENV = 'test';
+
+
+describe('Creating replies to comments', function () {
+    let server;
+    beforeEach(function () {
+        server = require('../app').server;
+        clearDatabase();
+    });
+    afterEach(function () {
+        clearDatabase();
+        server.close();
+
+    });
+    it('adds successfully new reply to comment', function (done) {
+        let user = createAndSaveUser();
+        let post = createPostForUser(user);
+
+        chai.request(server)
+            .post('/reply/')
+            .send({
+                content: 'test',
+                postId: post._id,
+                token: require('../src/routes/login').createToken(user)
+            })
+            .end(function (err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.have.property('success');
+                res.body.success.should.be.equal(true);
+                res.body.should.have.property('comment');
+                res.body.comment.should.have.property('content');
+                res.body.comment.content.should.be.equal('test');
+                res.body.comment.should.have.property('author');
+                done()
+            });
+    });
+});
+
+describe('Delete reply', function () {
+    let server;
+    beforeEach(function () {
+        server = require('../app').server;
+        clearDatabase();
+    });
+    afterEach(function () {
+        clearDatabase();
+        server.close();
+    });
+    it('removes successfully reply ', function (done) {
+        let user = createAndSaveUser();
+        let comment = createCommentForUser(user);
+        let post = createPostForUser(user, [comment]);
+        chai.request(server)
+            .delete('/comment/')
+            .send({
+                commentId: comment._id,
+                token: require('../src/routes/login').createToken(user)
+            })
+            .end(function (err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.have.property('success');
+                res.body.success.should.be.equal(true);
+                done()
+            });
+    });
+
+});
